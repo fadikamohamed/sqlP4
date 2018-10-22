7fb347c30464a564ec57e6f94f74f318dad4d7ec
fadikamohamed <fadika.mohamed02@gmail.com>
2018-10-22 09:24:31 +0200

    Partie 4 terminé

---
+++ sqlP4.sql
@@ -0,0 +1,51 @@
+-- Aller a la bdd webDevelopement --
+USE `webDevelopment`;
+
+-- Insérer dans la table languages les données JavaScript version 5 --
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (1, 'JavaScript', 5);
+
+-- Insérer dans la table languages les données PHP version 5.2 --
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (2, 'PHP', 5.2);
+
+-- Insérer dans la table languages les données PHP version 5.4 --
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (3, 'PHP', 5.4);
+
+-- Insérer dans la table languages les données HTML version 5.1 --
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (4, 'HTML', 5.1);
+
+-- Insérer dans la table languages les données JavaScript version 6 --
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (5, 'JavaScript', 6);
+
+-- Insérer dans la table languages les données JavaScript version 7 --
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (6, 'JavaScript', 7);
+
+-- Insérer dans la table languages les données JavaScript version 8 --
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (7, 'JavaScript', 8);
+
+-- Insérer dans la table languages les données PHP version 7 --
+INSERT INTO `languages` (`id`, `language`, `version`)
+VALUES (8, 'PHP', 7);
+
+
+-- Insérer dans la table frameworks les données Symfony version 2.8 --
+INSERT INTO `frameworks` (`id`, `framework`, `versions`)
+VALUES (1, 'Symphony', 2.8);
+
+-- Insérer dans la table frameworks les données Symfony version 3 --
+INSERT INTO `frameworks` (`id`, `framework`, `versions`)
+VALUES (2, 'Symphony', 3);
+
+-- Insérer dans la table frameworks les données Jquery version 1.6 --
+INSERT INTO `frameworks` (`id`, `framework`, `versions`)
+VALUES (3, 'Jquery', 1.6);
+
+-- Insérer dans la table frameworks les données Jquery version 2.10 --
+INSERT INTO `frameworks` (`id`, `framework`, `versions`)
+VALUES (4, 'Jquery', 2.10);
